b19ca7aae79fb319bf666ac7576f44dd9d8ff76b
yjsk250429 <yejin1046@gmail.com>
2025-06-27 17:34:43 +0900

    커밋2

--- js/common.js
+++ js/common.js
@@ -7,13 +7,95 @@ const preventDefaultAnchor = () => {
     window.scrollTo({ top:0, behavior: 'smooth' });
 };
 
+const MainMenu = () =>{
+    const $menu = getAll('#header .inner .nav .gnb > li > a');
+    const $lis = getAll('#header .inner .nav .gnb > li');
+    const $gnbs = getAll('#header .inner .nav .gnb > li > ul');
+
+    $menu.forEach((menutxt, idx)=>{
+        menutxt.addEventListener('click', (e)=>{
+            $lis.forEach((txt)=>{
+                txt.classList.remove('on');
+            });
+            $lis[idx].classList.add('on');
+        });
+    });
+
+    $lis.forEach((item, idx)=>{
+        item.addEventListener('mouseenter', (e)=>{
+            if ($gnbs[idx]) {
+            $gnbs[idx].classList.add('on');
+            $gnbs[idx].animate({opacity:[0, 1]},300);
+            }
+        });
+        item.addEventListener('mouseleave', (e)=>{
+            if ($gnbs[idx]) {
+            $gnbs[idx].animate({opacity:[1, 0]},300).finished.then(()=>{
+                $gnbs[idx].classList.remove('on');
+                });
+            }});
+        });
+};
+
+const MainBanner = () => {
+    const $bns = getAll('#visual .banner li');
+    const $pageTotal = get('#visual .inner .page-num span:last-child');
+    const $pageCount = get('#visual .inner .page-num span:first-child');
+    const $dots = getAll('#visual .inner .dots li');
+
+    let cnt = 0,
+        timer = null,
+        interval = 5000,
+        total = $bns.length;
+
+    $pageTotal.textContent = `${total>9?total:'0'+total}`;
+    $pageCount.textContent = `${total>9?cnt+1:'0'+(cnt+1)}` ;
+
+    const bannerSet = ()=>{
+        $bns.forEach((bn)=>{
+            bn.classList.remove('on');
+            $dots.forEach((dot)=>{
+                dot.classList.remove('on');
+            })
+        });
+        $bns[cnt].classList.add('on');
+        $dots[cnt].classList.add('on');
+        $pageCount.textContent = `${total>9?cnt+1:'0'+(cnt+1)}` ;
+    };
+
+    const make = () =>{
+        cnt++;
+        if (cnt > total -1) cnt=0;
+        bannerSet();
+    };
+    
+    timer = setInterval(make, interval);
+
+    $dots.forEach((dot, idx)=>{
+        dot.addEventListener('click', (e)=>{
+            cnt = idx;
+            bannerSet();
+            clearInterval(timer);
+            timer = setInterval(make, interval);
+        });
+    });
+};
+
 const topbar = () =>{
     const $topbtn = get('#wrap .top');
+    const $header = get('#header');
+
     let ty = 0;
     window.addEventListener('scroll', (e)=>{
         ty = window.scrollY;
-        console.log(ty);
-        if (ty > 400){
+        if (ty > 50){
+            $header.classList.add('on');
+        }else{
+            $header.classList.remove('on');
+
+        }
+
+        if (ty > 200){
             $topbtn.classList.add('on');
         } else{
             $topbtn.classList.remove('on');
@@ -23,9 +105,44 @@ const topbar = () =>{
         });
     });
 };
+    //클릭 할때마다 size++ 되고, 그러다가 ul의 길이를 초과하게 되면 멈춤
+const con3_partners = () => {
+    const $prev = get('.main .con2 .inner .btns .prev');
+    const $next = get('.main .con2 .inner .btns .next');
+    const $box = get('.main .con2 .inner ul');
+
+    let size = 259,
+        current = 0;
+
+    $next.addEventListener('click', (e)=>{
+        current += size ;
+        if(current > 3700) {
+            return;
+        }else{
+            $box.style.left = `-${current}px`;
+        };
+    });
+};
+
+const con3_sel = ()=>{
+    const $sel = getAll('.main .con3 .inner .sel li');
+
+    $sel.forEach((item)=>{
+        item.addEventListener('click', (e)=>{
+            $sel.forEach((itemLi)=>{
+                itemLi.classList.remove('on');
+            });
+            item.classList.add('on');
+        });
+    });
+};
 
 const comInit = () => {
     topbar();
+    MainBanner();
+    MainMenu();
+    con3_partners();
+    con3_sel();
 };
 
 (() => {
